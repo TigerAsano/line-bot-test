885fb938e28e21c282091d6eac4e3bf424d1f1eb
daltonya <96973600+TigerAsano@users.noreply.github.com>
2022-01-12 11:44:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,172 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+from linebot import LineBotApi
+from linebot.models import TextSendMessage,  FlexSendMessage
+
+def replay-message(event):
+
+    payload = {
+    "type": "flex",
+    "altText": "Flex Message",
+    "contents": {
+        "type": "bubble",
+        "hero": {
+        "type": "image",
+        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+            "type": "uri",
+            "label": "Line",
+            "uri": "https://linecorp.com/"
+        }
+        },
+        "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+            "type": "text",
+            "text": "Brown Cafe",
+            "size": "xl",
+            "weight": "bold"
+            },
+            {
+            "type": "box",
+            "layout": "baseline",
+            "margin": "md",
+            "contents": [
+                {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                "size": "sm"
+                },
+                {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                "size": "sm"
+                },
+                {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                "size": "sm"
+                },
+                {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                "size": "sm"
+                },
+                {
+                "type": "icon",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                "size": "sm"
+                },
+                {
+                "type": "text",
+                "text": "4.0",
+                "flex": 0,
+                "margin": "md",
+                "size": "sm",
+                "color": "#999999"
+                }
+            ]
+            },
+            {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "margin": "lg",
+            "contents": [
+                {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                    {
+                    "type": "text",
+                    "text": "Place",
+                    "flex": 1,
+                    "size": "sm",
+                    "color": "#AAAAAA"
+                    },
+                    {
+                    "type": "text",
+                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                    "flex": 5,
+                    "size": "sm",
+                    "color": "#666666",
+                    "wrap": True
+                    }
+                ]
+                },
+                {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                    {
+                    "type": "text",
+                    "text": "Time",
+                    "flex": 1,
+                    "size": "sm",
+                    "color": "#AAAAAA"
+                    },
+                    {
+                    "type": "text",
+                    "text": "10:00 - 23:00",
+                    "flex": 5,
+                    "size": "sm",
+                    "color": "#666666",
+                    "wrap": True
+                    }
+                ]
+                }
+            ]
+            }
+        ]
+        },
+        "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "flex": 0,
+        "spacing": "sm",
+        "contents": [
+            {
+            "type": "button",
+            "action": {
+                "type": "uri",
+                "label": "CALL",
+                "uri": "https://linecorp.com"
+            },
+            "height": "sm",
+            "style": "link"
+            },
+            {
+            "type": "button",
+            "action": {
+                "type": "uri",
+                "label": "WEBSITE",
+                "uri": "https://linecorp.com"
+            },
+            "height": "sm",
+            "style": "link"
+            },
+            {
+            "type": "spacer",
+            "size": "sm"
+            }
+        ]
+        }
+    }
+    }
+
+    container_obj = FlexSendMessage.new_from_json_dict(payload)
+
+    line_bot_api.reply_massage(
+        event.reply_token
+        , messages=container_obj)
 
 if __name__ == "__main__":
 #    app.run()
